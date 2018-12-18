e0a22c096c9927508db35b5bedbf90e64a2a6d23
hadilo <devhadi@gmail.com>
2018-12-18 19:46:24 +0700

    1. implment popup

--- app/src/main/java/com/hadilo/spinnertextinputlayout/MainActivity.java
+++ app/src/main/java/com/hadilo/spinnertextinputlayout/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     SpinnerTextInputLayout spinnerGender;
+    SpinnerTextInputLayout spinnerColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinnerGender  = findViewById(R.id.spinner_gender);
+        spinnerColor  = findViewById(R.id.spinner_color);
 
+
+        spinnerGender.setMode(SpinnerTextInputLayout.MODE_DROPDOWN);
         spinnerGender.setItems(getGenderList());
         spinnerGender.setOnItemSelectedListener(new SpinnerTextInputLayout.OnItemSelectedListener<SpinnerTextInputLayout.Listable>() {
             @Override
@@ -29,12 +33,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        spinnerColor.setMode(SpinnerTextInputLayout.MODE_POPUP);
+        spinnerColor.setItems(getColorList());
+        spinnerColor.setOnItemSelectedListener(new SpinnerTextInputLayout.OnItemSelectedListener<SpinnerTextInputLayout.Listable>() {
+            @Override
+            public void onItemSelectedListener(SpinnerTextInputLayout.Listable item, int selectedIndex) {
+                spinnerColor.getEditText().setText(item.getLabel());
+                spinnerColor.setErrorEnabled(false);
+                spinnerColor.setError("");
+            }
+        });
     }
 
     private List<SpinnerTextInputLayout.Listable> getGenderList(){
         List<SpinnerTextInputLayout.Listable> genders = new ArrayList<>();
         genders.add(new SpinnerTextInputLayout.ItemModel("Laki-Laki"));
         genders.add(new SpinnerTextInputLayout.ItemModel("Perempuan"));
+        return genders;
+    }
+
+    private List<SpinnerTextInputLayout.Listable> getColorList(){
+        List<SpinnerTextInputLayout.Listable> genders = new ArrayList<>();
+        genders.add(new SpinnerTextInputLayout.ItemModel("Merah"));
+        genders.add(new SpinnerTextInputLayout.ItemModel("Kuning"));
+        genders.add(new SpinnerTextInputLayout.ItemModel("Hijau"));
         return genders;
     }
 }
