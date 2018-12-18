c43c1b31236966f15c695c262a6812ec1c8fab57
hadilo <devhadi@gmail.com>
2018-12-18 18:45:00 +0700

    1. implement SpinnerTextInputLayout and example 2. add ic_baseline_arrow_drop_down_black_xx.png

--- app/src/main/java/com/hadilo/spinnertextinputlayout/MainActivity.java
+++ app/src/main/java/com/hadilo/spinnertextinputlayout/MainActivity.java
@@ -3,11 +3,38 @@ package com.hadilo.spinnertextinputlayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hadilo.spinnertextinputlayout.widget.spinner.SpinnerTextInputLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    SpinnerTextInputLayout spinnerGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerGender  = findViewById(R.id.spinner_gender);
+
+        spinnerGender.setItems(getGenderList());
+        spinnerGender.setOnItemSelectedListener(new SpinnerTextInputLayout.OnItemSelectedListener<SpinnerTextInputLayout.Listable>() {
+            @Override
+            public void onItemSelectedListener(SpinnerTextInputLayout.Listable item, int selectedIndex) {
+                spinnerGender.getEditText().setText(item.getLabel());
+                spinnerGender.setErrorEnabled(false);
+                spinnerGender.setError("");
+            }
+        });
+
+    }
+
+    private List<SpinnerTextInputLayout.Listable> getGenderList(){
+        List<SpinnerTextInputLayout.Listable> genders = new ArrayList<>();
+        genders.add(new SpinnerTextInputLayout.ItemModel("Laki-Laki"));
+        genders.add(new SpinnerTextInputLayout.ItemModel("Perempuan"));
+        return genders;
     }
 }
